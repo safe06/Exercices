231b701764ac83134946bab1dddb3a67f2da6574
safe06 <safa.benchouaieb@gmail.com>
2023-12-19 11:28:51 +0100

    voila les exercices

---
+++ src/Exo1/App.java
@@ -0,0 +1,45 @@
+package Exo1 ;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in) ;
+
+        //Exercice 1 :
+        //Écrire un programme en Java qui demande l'âge d'un enfant et permet d'informer de sa catégorie sachant que les catégories sont les suivantes: 
+        //"poussin de 6 a 7 ans"
+        //"pupille de 8 a 9 ans "
+        //"minime de 10 a 11 ans "
+        //" cadet après 12 ans ".
+        System.out.println("saisie un 1 ere nombre");
+        Integer u = scanner.nextInt();
+        System.out.println(u);
+
+        switch (u) {
+            case 6, 7: 
+                System.out.println("poussin");
+                break;
+            case 8, 9:
+                System.out.println("pupille");
+                break;
+            case 10, 11:
+                System.out.println("minime");
+                break;
+            default:
+                System.out.println("cadet");
+                break;
+        }
+
+
+        scanner.close();
+
+
+
+
+
+
+    }
+}
